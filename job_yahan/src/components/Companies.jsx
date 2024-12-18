518534f36196686c509580686c3a548aca32d13f
Er.Krishna Kumar <135963785+Krishnarov@users.noreply.github.com>
2024-12-18 14:34:51 +0530

    try to best

--- job_yahan/src/components/Companies.jsx
+++ job_yahan/src/components/Companies.jsx
@@ -1,27 +1,38 @@
 import React from "react";
+import Slider from "react-slick";
+function Companies(props) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
 
-function Companies() {
-  const CompaniesData = [
-    { name: "Vodafone Idea", title: "Vodafone Idea Limited", logo: "https://www.myvi.in/content/dam/vodafoneideadigital/homespyder/Vi-logo.svg" },
-    { name: "Intel", title: "Intel", logo: "https://www.myvi.in/content/dam/vodafoneideadigital/homespyder/Vi-logo.svg" },
-    { name: "Tesla", title: "Automotive company", logo: "https://www.myvi.in/content/dam/vodafoneideadigital/homespyder/Vi-logo.svg" },
-    { name: "AMD", title: "IT corporation", logo: "https://www.myvi.in/content/dam/vodafoneideadigital/homespyder/Vi-logo.svg" },
-    { name: "Tata Consultancy Services", title: "IT company", logo:"https://www.myvi.in/content/dam/vodafoneideadigital/homespyder/Vi-logo.svg" },
-  ];
+    pauseOnHover: true,
+  };
+
   return (
-    <div className="px-28 py-4">
+    <div className="md:px-28 px-4 py-4 overflow-hidden">
       <p className="text-sm">Companies we helped grow</p>
-      <div className="grid grid-cols-5 py-4 gap-6">
-      {CompaniesData.map((data,index)=>(
-        <div key={index} className=" text-xs text-center">
-            <div className="flex justify-center">
-            <img className="aspect-square object-cover object-center" src={data.logo} alt={data.name} />
+      <div className=" py-4 gap-6 slider-container">
+        <Slider {...settings}>
+          {props.comp.map((data, index) => (
+            <div key={index} className=" text-xs text-center cursor-pointer">
+              <div className="flex justify-center">
+                <img
+                  className="aspect-square object-cover object-center"
+                  src={data.logo}
+                  alt={data.name}
+                />
+              </div>
+              {data.name}
             </div>
-            {data.name}
-        </div>
-      ))}
+          ))}
+        </Slider>
       </div>
-      
     </div>
   );
 }
--- job_yahan/src/components/NavBar.jsx
+++ job_yahan/src/components/NavBar.jsx
@@ -1,22 +1,44 @@
 import React, { useEffect, useState } from "react";
+import Login from "./Login";
+import { Link } from "react-router-dom";
+import Menu from "./menu";
 
 function NavBar() {
-  const [sticky ,setSticky]=useState(false)
-  useEffect(()=>{
-    const heandlscroll=()=>{
-      if(window.scrollY>0){
-        setSticky(true)
-      }else{
-        setSticky(false)
+  const [sticky, setSticky] = useState(false);
+  const [menu,setmenu]=useState(false)
+  useEffect(() => {
+    const heandlscroll = () => {
+      if (window.scrollY > 0) {
+        setSticky(true);
+        setmenu(false)
+      } else {
+        setSticky(false);
+        setmenu(false)
+
       }
-    }
-    window.addEventListener("scroll",heandlscroll)
-    return ()=>{
-      window.addEventListener("scroll",heandlscroll)
-    }
-  },[])
+    };
+    window.addEventListener("scroll", heandlscroll);
+    return () => {
+      window.addEventListener("scroll", heandlscroll);
+    };
+
+
+  }, []);
+
+function menuhandel() {
+  if(menu===true){
+    setmenu(false)
+
+  }else{
+    setmenu(true)
+  }
+}
   return (
-    <div className={`py-2 px-28 bg-slate-100 fixed top-0 left-0 right-0 z-50 ${sticky ? " shadow-md duration-300 transition-all ease-in-out":""}`}>
+    <div
+      className={`py-2 md:px-28 px-4 bg-slate-100 fixed top-0 left-0 right-0 z-50 ${
+        sticky ? " shadow-md duration-300 transition-all ease-in-out" : ""
+      }`}
+    >
       <div className="flex items-center justify-between">
         <div className="flex items-center">
           <div className="px-4 py-1 bg-indigo-600 text-white rounded-full font-black text-xl">
@@ -24,16 +46,39 @@ function NavBar() {
           </div>
           <div className="font-black text-2xl ml-3">JOB_YAHNA</div>
 
-          <div className="flex items-center gap-4 ml-8">
-            <div>Find Jobs</div>
-            <div>Browse Companies</div>
-          </div>
+          {/* <div className="md:flex items-center gap-4 ml-8 md:block hidden">
+            <div>
+              <a href="">Find Jobs</a>
+            </div>
+            <div>
+              <a href="">Browse Companies</a>
+            </div>
+          </div> */}
         </div>
-        <div className="flex items-center gap-4">
-            <button className="bg-slate-100 text-indigo-600 hover:bg-indigo-600 hover:text-white border-2 border-indigo-600 ">Login</button>
-            <button className="bg-indigo-600 hover:text-indigo-600  text-white hover:bg-transparent border-2 border-indigo-600 ">Signup</button>
+        <div className="md:flex items-center gap-4 hidden md:block">
+          <button
+            className="bg-slate-100 text-indigo-600 hover:bg-indigo-600 hover:text-white border-2 border-indigo-600 "
+            onClick={() => document.getElementById("my_modal_3").showModal()}
+          >
+            Login
+          </button>
+          <Login />
+            <Link  to="/signup">
+          <button className="bg-indigo-600 hover:text-indigo-600  text-white hover:bg-transparent border-2 border-indigo-600 ">
+            Signup
+          </button>
+            </Link>
+        </div>
+        <div className="border rounded-full bg-white flex items-center justify-center w-10 h-10 md:hidden block" onClick={()=>menuhandel() }>
+          {menu===false? <i className="ri-menu-fill"></i>:"X"}
+
         </div>
       </div>
+      <div className="md:hidden block text-right absolute right-5  ">
+          {menu===true? <Menu />:null}
+      </div>
+
+      
     </div>
   );
 }
